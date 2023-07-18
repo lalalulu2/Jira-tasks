132e0c50e8d2c2c1742273254df6a9fa81b90dca
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:36:02 +0300

    Create TypeJira.java

---
+++ src/main/java/ru/savadevel/issuestojira/model/TypeJira.java
@@ -0,0 +1,33 @@
+package ru.savadevel.issuestojira.model;
+
+/**
+ * Типы задач в Jira, задается ID типа в Jira
+ */
+public enum TypeJira {
+    TASK(10001L),
+    EPIC(10002L),
+    SUBTASK(10003L);
+
+    private final Long id;
+
+    TypeJira(Long id) {
+        this.id = id;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String value() {
+        return name();
+    }
+
+    public static TypeJira fromValue(Long id) {
+        for (TypeJira c : TypeJira.values()) {
+            if (c.id.equals(id)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(String.valueOf(id));
+    }
+}
