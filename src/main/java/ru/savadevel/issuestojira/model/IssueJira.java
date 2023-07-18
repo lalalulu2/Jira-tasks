31838814059c9325b8c58c4934d319b54d5d928a
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:34:44 +0300

    Create IssueJira.java

---
+++ src/main/java/ru/savadevel/issuestojira/model/IssueJira.java
@@ -0,0 +1,21 @@
+package ru.savadevel.issuestojira.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+/**
+ * Задача в Jira
+ */
+@Data
+@AllArgsConstructor
+public class IssueJira {
+
+    @NonNull
+    private String projectKey;
+    @NonNull
+    private Integer id;
+    private TypeJira type;
+    private PriorityJira priority;
+    private String summary;
+}
