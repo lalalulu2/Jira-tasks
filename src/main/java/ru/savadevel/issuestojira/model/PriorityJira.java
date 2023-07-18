bff1e6cb72a3834d7360b156c2bf17a7e0fe5a98
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:35:18 +0300

    Create PriorityJira.java

---
+++ src/main/java/ru/savadevel/issuestojira/model/PriorityJira.java
@@ -0,0 +1,35 @@
+package ru.savadevel.issuestojira.model;
+
+/**
+ * Приоритет задачи в Jira, задается ID приоритета в Jira
+ */
+public enum PriorityJira {
+    HIGHEST(1L),
+    HIGH(2L),
+    MEDIUM(3L),
+    LOW(4L),
+    LOWEST(5L);
+
+    private final Long id;
+
+    PriorityJira(Long id) {
+        this.id = id;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String value() {
+        return name();
+    }
+
+    public static PriorityJira fromValue(Long id) {
+        for (PriorityJira c : PriorityJira.values()) {
+            if (c.id.equals(id)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(String.valueOf(id));
+    }
+}
