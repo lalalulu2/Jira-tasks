df8940e449d7ddb62b82e0c505ae957e0caecf52
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:37:31 +0300

    Create DescriptionTo.java

---
+++ src/main/java/ru/savadevel/issuestojira/to/DescriptionTo.java
@@ -0,0 +1,25 @@
+package ru.savadevel.issuestojira.to;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * Описание задачи
+ */
+@Data
+@NoArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DescriptionTo {
+
+    @XmlElement(name = "id")
+    @NonNull
+    private Integer id;
+
+    @XmlElement(name = "summary")
+    private String summary;
+}
