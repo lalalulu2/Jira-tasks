ee4d668cc6b900f938d92acb28b42cf311715ae4
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:37:55 +0300

    Create FormTo.java

---
+++ src/main/java/ru/savadevel/issuestojira/to/FormTo.java
@@ -0,0 +1,29 @@
+package ru.savadevel.issuestojira.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Параметры формы загрузки задач в Jira, включает параметры подключения к Jira и задачи, которые требуется создать
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FormTo {
+
+    @NotBlank(message = "URL of Jira is mandatory")
+    private String url;
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+    @NotNull
+    private MultipartFile file;
+}
