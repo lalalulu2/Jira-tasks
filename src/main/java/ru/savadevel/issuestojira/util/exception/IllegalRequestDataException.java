be23305d714b105d62a9146d060fc7e410bc28d8
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-07-18 23:43:12 +0300

    Create IllegalRequestDataException.java

---
+++ src/main/java/ru/savadevel/issuestojira/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,31 @@
+package ru.savadevel.issuestojira.util.exception;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindingResult;
+
+import java.util.stream.Collectors;
+
+/**
+ * Исключение при обработке входных параметров
+ */
+public class IllegalRequestDataException extends RuntimeException {
+
+    private final transient BindingResult bindingResult;
+
+    public IllegalRequestDataException(BindingResult bindingResult) {
+        super(bindingResult.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("; ")));
+        this.bindingResult = bindingResult;
+    }
+
+    /**
+     * Возвращает результаты проверки входных параметров
+     *
+     * @return результаты проверки входных параметров
+     */
+    public BindingResult getBindingResult() {
+        return bindingResult;
+    }
+}
